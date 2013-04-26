b08614dc1bfe647790191fca703a8486db97d20d
Henri Chataing <chataing.henri@gmail.com>
2013-04-26 18:04:56 -0300

    Nothing much

--- Quipper.hs
+++ Quipper.hs
@@ -54,7 +54,7 @@ main = do
 
   if inter opt then
     mapM (\e -> do
-                  putStrLn (let (_, ctx) = Interpret.run e newContext in show $ circuit ctx)) progs
+             putStrLn (let (v, ctx) = Interpret.run e newContext in (show v))) progs
   else
     return [()]
 
