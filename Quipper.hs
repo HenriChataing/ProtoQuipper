c60086ef56ffe021c44b4ca4742537339a06bdfc
Henri Chataing <chataing.henri@gmail.com>
2013-05-25 21:51:30 -0300

    Use the newly developped Args module to parse arguments

--- Quipper.hs
+++ Quipper.hs
@@ -5,6 +5,7 @@ import Lexer
 
 import Classes
 import Utils
+import Args
 
 import Syntax
 import Printer
@@ -20,41 +21,54 @@ import System.IO
 import System.Environment
 
 data Options = Opt {
-  inter :: Bool,
-  typ :: Bool,
-  printp :: Bool,
-  test :: Bool,
+  -- Current options
+  do_interpret :: Bool,
+  do_typing :: Bool,
+  do_printing :: Bool,
+  do_testing :: Bool, testing_lvl :: Int,
   filename :: String
 }
 
-initOptions = Opt {
-  inter = False,
-  typ = False,
-  printp = False,
-  test = False,
+-- Some setters
+set_interpret :: Options -> Options
+set_typing :: Options -> Options
+set_printing :: Options -> Options
+set_testing :: Int -> Options -> Options
+parse_file :: String -> Options -> Options
+--------------------------------
+set_interpret opt = opt { do_interpret = True }
+set_typing opt = opt { do_typing = True }
+set_printing opt = opt { do_printing = True }
+set_testing n opt = opt { do_testing = True, testing_lvl = n }
+parse_file f opt = opt { filename = f }
+
+-- Intitial state
+init_options = Opt {
+  do_interpret = False,
+  do_typing = False,
+  do_printing = False,
+  do_testing = False, testing_lvl = 0,
   filename = ""
 }
 
-options :: [(String, Options -> IO Options)]
-options = [
-  ("-i", (\opt -> return opt { inter = True })),
-  ("-t", (\opt -> return opt { typ = True })),
-  ("-p", (\opt -> return opt { printp = True })),
-  ("-test", (\opt -> return opt { test = True })) ]
+-- Option continuations
+option_defs :: ParseResult Options ()
+option_defs = do
+  when_parse "-i" (do apply set_interpret)
+  when_parse "-t" (do apply set_typing)
+  when_parse "-p" (do apply set_printing)
+  when_parse "-test" (do
+      s <- next
+      n <- return (read s :: Int)
+      apply $ set_testing n)
+  when_default (\s -> do apply $ parse_file s)
 
-parseOptions :: [String] -> IO Options
-parseOptions [] =
-  return initOptions
-parseOptions (s:cs) = do
-  opt <- parseOptions cs
-  case lookup s options of
-    Just f -> f opt
-    Nothing -> return opt { filename = s }
+-------------------------------------------------------------
 
 main = do
   -- Parse program options
   args <- getArgs
-  opt <- parseOptions args
+  opt <- return $ run_parser init_options args option_defs
 
   -- Lex and parse file
   putStrLn $ "\x1b[1;34m" ++ "## Parsing content of file " ++ filename opt ++ "\x1b[0m"
@@ -62,7 +76,7 @@ main = do
   prog <- return (parse $ mylex (filename opt) contents)
 
   -- Actions
-  if printp opt then do
+  if do_printing opt then do
     putStrLn $ "\x1b[1;33m" ++ ">> Printing" ++ "\x1b[0m"
     putStrLn $ "\x1b[1m" ++ "Surface syntax :" ++ "\x1b[0m"
     putStrLn $ pprint (clear_location prog)
@@ -71,7 +85,7 @@ main = do
   else
     return ()
 
-  if inter opt then do
+  if do_interpret opt then do
     putStrLn $ "\x1b[1;33m" ++ ">> Interpret" ++ "\x1b[0m" 
     case Interpret.run (drop_constraints prog) of
       Ok v -> do
@@ -82,7 +96,7 @@ main = do
   else
     return ()
 
-  if typ opt then do
+  if do_typing opt then do
     putStrLn $ "\x1b[1;33m" ++ ">> Typing" ++ "\x1b[0m"
     putStrLn $ "\x1b[1m" ++ "TypeInference :" ++ "\x1b[0m"
 --    putStrLn $ pprint_constraints $ type_inference prog
@@ -90,7 +104,7 @@ main = do
   else
     return ()
 
-  if test opt then do
+  if do_testing opt then do
     putStrLn $ "\x1b[1;33m" ++ ">> Typing test" ++ "\x1b[0m"
     putStrLn $ "\x1b[1m" ++ "Test TypeInference :" ++ "\x1b[0m"
 --    putStrLn $ pprint_constraints $ type_inference prog
