8d697897f614d973e31420d20d60569e4fd41149
Henri Chataing <chataing.henri@gmail.com>
2013-07-12 15:55:14 -0300

    Move the priting of core types and expressions to another module CorePrinter

---
+++ typing/CorePrinter.hs
@@ -0,0 +1,164 @@
+-- | This module provides the pretty printing functions necessary to render
+-- the types and expressions of the core syntax
+
+module CorePrinter where
+
+import Classes
+import Utils
+
+import CoreSyntax
+
+import Text.PrettyPrint.HughesPJ as PP
+import Data.List as List
+
+instance PPrint LinType where
+  -- Print unto Lvl = n
+  sprintn _ (TVar x) = subvar 'X' x
+  sprintn _ TUnit = "T"
+  sprintn _ TBool = "bool"
+  sprintn _ TQbit = "qbit"
+  sprintn _ (TUser n) = n
+  sprintn (Nth 0) _ = "..."
+
+  sprintn lv (TTensor (a:rest)) =
+    let dlv = decr lv in
+    (case a of
+       TBang _ (TArrow _ _) -> "(" ++ sprintn dlv a ++ ")"
+       TBang _ (TTensor _) -> "(" ++ sprintn dlv a ++ ")"
+       _ -> sprintn dlv a) ++
+    List.foldl (\s b -> s ++ " * " ++
+                  (case b of
+                     TBang _ (TArrow _ _) -> "(" ++ sprintn dlv b ++ ")"
+                     TBang _ (TTensor _) -> "(" ++ sprintn dlv b ++ ")"
+                     _ -> sprintn dlv b)) "" rest
+
+  sprintn lv (TArrow a b) =
+    let dlv = decr lv in
+    (case a of
+       TBang _ (TArrow _ _) -> "(" ++ sprintn dlv a ++ ")"
+       _ -> sprintn dlv a) ++ " -> " ++
+    sprintn dlv b
+
+  sprintn lv (TCirc a b) =
+    let dlv = decr lv in
+    "circ(" ++ sprintn dlv a ++ ", " ++ sprintn dlv b ++ ")"
+
+  -- Print unto Lvl = +oo
+  pprint a = sprintn Inf a
+
+  -- Print unto Lvl = default
+  sprint a = sprintn defaultLvl a
+
+
+instance PPrint Type where
+  sprintn lv (TBang f a) =
+    let pf = pprint f in
+    if List.length pf == 0 then
+      pf ++ sprintn lv a
+    else
+      pf ++ (case a of
+               (TTensor _) -> "(" ++ sprintn lv a ++ ")"
+               (TArrow _ _) -> "(" ++ sprintn lv a ++ ")"
+               _ -> sprintn lv a)
+ 
+  -- Print unto Lvl = +oo
+  pprint a = sprintn Inf a
+
+  -- Print unto Lvl = default
+  sprint a = sprintn defaultLvl a
+
+
+
+instance PPrint Pattern where
+   -- Print unto Lvl = n
+  sprintn _ (PVar x) = subvar 'x' x
+  sprintn _ PUnit = "<>"
+  sprintn (Nth 0) _ = "..."
+
+  sprintn lv (PTuple (p:rest)) =
+    let dlv = decr lv in
+    "<" ++ sprintn dlv p ++ List.foldl (\s q -> s ++ ", " ++ sprintn dlv q) "" rest ++ ">"
+
+  sprintn lv (PData dcon p) =
+    subvar 'D' dcon ++ "(" ++ sprintn (decr lv) p ++ ")"
+
+  sprintn lv (PLocated p _) =
+    sprintn lv p
+
+  -- Print unto Lvl = +oo
+  pprint a = sprintn Inf a
+
+  -- Print unto Lvl = default
+  sprint a = sprintn defaultLvl a
+
+
+-- | Print an expression as a pp document
+print_doc :: Expr -> Doc
+print_doc EUnit = text "<>"
+print_doc (EBool b) = if b then text "true" else text "false"
+print_doc (EVar x) = text $ subvar 'x' x
+
+print_doc (ELet p e f) =
+  text "let" <+> text (pprint p) <+> equals <+> print_doc e <+> text "in" $$
+  print_doc f
+
+print_doc (ETuple elist) =
+  let plist = List.map print_doc elist in
+  let slist = (punctuate comma $ List.init plist) ++ [List.last plist] in
+  char '<' <+> hsep slist <+> char '>'
+
+print_doc (EApp e f) =
+  let pe = print_doc e
+      pf = print_doc f in
+  (case e of
+     EFun _ _ -> parens pe
+     _ -> pe) <+> 
+  (case f of
+     EFun _ _ -> parens pf
+     EApp _ _ -> parens pf
+     _ -> pf)
+
+print_doc (EFun p e) =
+  text "fun" <+> text (pprint p) <+> text "->" $$
+  nest 2 (print_doc e)
+
+print_doc (EIf e f g) =
+  text "if" <+> print_doc e <+> text "then" $$
+  nest 2 (print_doc f) $$
+  text "else" $$
+  nest 2 (print_doc g)
+
+print_doc (EBox t) =
+  text "box" <> brackets (text $ pprint t)
+
+print_doc EUnbox =
+  text "unbox"
+
+print_doc ERev =
+  text "rev"
+
+print_doc (EData datacon e) =
+  let pe = print_doc e in
+  text (subvar 'D' datacon) <+> (case e of
+                            EBool _ -> pe
+                            EUnit -> pe
+                            EVar _ -> pe
+                            _ -> parens pe)
+
+print_doc (EMatch e blist) =
+  text "match" <+> print_doc e <+> text "with" $$
+  nest 2 (List.foldl (\doc (p, f) ->
+                        let pmatch = char '|' <+> text (pprint p) <+> text "->" <+> print_doc e in
+                        if isEmpty doc then
+                          pmatch
+                        else
+                          doc $$ pmatch) PP.empty blist)
+
+print_doc (ELocated e _) =
+  print_doc e
+
+instance PPrint Expr where
+  sprintn lv e = PP.render $ print_doc e
+  sprint e = sprintn defaultLvl e
+  pprint e = sprintn Inf e
+
