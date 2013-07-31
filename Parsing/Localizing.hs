723851b51bf08cab5bbf14d1e25f9350b25007f7
Henri Chataing <chataing.henri@gmail.com>
2013-07-31 12:07:43 -0300

    Write more Haddock formatted comments, in modules Lexer and Localizing

--- Parsing/Localizing.hs
+++ Parsing/Localizing.hs
@@ -1,25 +1,27 @@
--- | This module provides types and functions useful to deal with location in files
--- The location is represented by an extent, which is an interval between two locii
-
+-- | This module provides types and functions useful to work with location in files.
+-- A location is represented by an extent, which is an interval between two locii.
 module Parsing.Localizing where
 
-
 -- | Definition of a locus as a point in a file located
--- by its line and column numbers
+-- by its line and column numbers.
 data Locus = Loc {
-  line :: Int,
-  column :: Int
+  line :: Int,     -- ^ Line number.
+  column :: Int    -- ^ Column number.
 } deriving Eq
 
 
--- | Definition of an extent as an interval delimited by two locii
+-- | Locii are ordered using the lexical order on pairs (line, column).
+instance Ord Locus where
+  compare l1 l2 = compare (line l1, column l1) (line l2, column l2)
+
+
+-- | Definition of an extent as an interval delimited by two locii.
 data Extent = Ext {
-  lbegin :: Locus,
-  lend :: Locus
+  lbegin :: Locus,   -- ^ Beginning of the extent.
+  lend :: Locus      -- ^ End of the extent.
 } deriving Eq
 
 
--- | Pretty printing of an extent
 instance Show Extent where
   show ex =
     if (line $ lbegin ex) == (line $ lend ex) then
@@ -33,41 +35,37 @@ instance Show Extent where
        (show $ column $ lend ex))
 
 
--- | Default locus : line and column numbers are 0
+-- | Default locus : line and column numbers are set to 0.
 locus_unknown :: Locus
 locus_unknown =
   Loc { line = 0, column = 0 }
 
 
--- | Default extent : delimited by the default locus
+-- | Default extent : delimited by the default locus.
 extent_unknown :: Extent
 extent_unknown =
   Ext { lbegin = locus_unknown, lend = locus_unknown }
 
 
--- | Return the union of two extents. The extent by be given in an ordered fashion
+-- | Return the union of two extents.
 fromto :: Extent -> Extent -> Extent
 fromto ex1 ex2 =
-  Ext { lbegin = Loc { line = line $ lbegin ex1, column = column $ lbegin ex1 },
-        lend = Loc { line = line $ lend ex2, column = column $ lend ex2 } }
+  let begin = min (lbegin ex1) (lbegin ex2)
+      end = max (lend ex1) (lend ex2) in
+  Ext { lbegin = begin, lend = end }
 
 
--- | Similar to fromto, but with optional arguments, resulting in
--- an optional result
+-- | Similar to fromto, but with optional arguments, resulting in an optional result.
 fromto_opt :: Maybe Extent -> Maybe Extent -> Maybe Extent
 fromto_opt _ Nothing = Nothing
 fromto_opt Nothing _ = Nothing
 fromto_opt (Just ex1) (Just ex2) = Just (fromto ex1 ex2)
 
 
--- | Class of objects that can be located in a file
---   locate returns the object located at extent ext
---   locate_opt do the same as locate, with an optional location
---   clear_location removes all location annotations
---   location returns the assiocated location, if any
+-- | Class of objects that can hold locations in files.
 class Located a where
-  locate :: a -> Extent -> a
-  locate_opt :: a -> Maybe Extent -> a
-  clear_location :: a -> a
-  location :: a -> Maybe Extent
+  locate :: a -> Extent -> a               -- ^ Annotates an object with a location.
+  locate_opt :: a -> Maybe Extent -> a     -- ^ Same as locate, with an optional location.
+  clear_location :: a -> a                 -- ^ Removes all location annotations.
+  location :: a -> Maybe Extent            -- ^ Returns the location associated to an object, if any.
 
