a9acf78beec38661827894d719530b5da8741c27
Peter Selinger <selinger@mathstat.dal.ca>
2013-08-20 12:11:53 -0300

    Documentation edits: Parsing and Utils

--- Parsing/Location.hs
+++ Parsing/Location.hs
@@ -1,9 +1,9 @@
--- | This module provides types and functions useful to work with location in files.
--- A location is represented by an extent, which is an interval between two loci.
+-- | This module provides types and functions for recording and manipulating locations in files.
+-- A location is represented by an /extent/, which is an interval between two loci.
 module Parsing.Location where
 
--- | Definition of a locus as a point in a file located
--- by its line and column numbers.
+-- | A /locus/ is a point in a file, identified by its line and column
+-- numbers.
 data Locus = Loc {
   line :: Int,     -- ^ Line number.
   column :: Int    -- ^ Column number.
@@ -15,7 +15,7 @@ instance Ord Locus where
   compare l1 l2 = compare (line l1, column l1) (line l2, column l2)
 
 
--- | Definition of an extent as an interval delimited by two loci.
+-- | An /extent/ is an interval delimited by two loci.
 data Extent = Ext {
   lbegin :: Locus,   -- ^ Beginning of the extent.
   lend :: Locus      -- ^ End of the extent.
@@ -35,26 +35,25 @@ instance Show Extent where
        (show $ column $ lend ex))
 
 
--- | Default locus: line and column numbers are set to 0.
+-- | The default locus: line and column numbers are set to 0.
 locus_unknown :: Locus
 locus_unknown =
   Loc { line = 0, column = 0 }
 
 
--- | Default extent: delimited by the default locus.
+-- | The default extent: delimited by the default locus.
 extent_unknown :: Extent
 extent_unknown =
   Ext { lbegin = locus_unknown, lend = locus_unknown }
 
 
-
--- | Default file name: \"*Unknown*\"
+-- | The default file name: @\"*Unknown*\"@.
 file_unknown :: String
 file_unknown =
   "*Unknown*"
 
 
--- | Returns the union of two extents.
+-- | Return the union of two extents.
 fromto :: Extent -> Extent -> Extent
 fromto ex1 ex2 =
   let begin = min (lbegin ex1) (lbegin ex2)
@@ -62,17 +61,24 @@ fromto ex1 ex2 =
   Ext { lbegin = begin, lend = end }
 
 
--- | Similar to fromto, but with optional arguments, resulting in an optional result.
+-- | Like 'fromto', but with optional arguments and an optional result.
 fromto_opt :: Maybe Extent -> Maybe Extent -> Maybe Extent
 fromto_opt _ Nothing = Nothing
 fromto_opt Nothing _ = Nothing
 fromto_opt (Just ex1) (Just ex2) = Just (fromto ex1 ex2)
 
 
--- | Class of objects that can hold locations in files.
+-- | A type class for objects that can be /located/, i.e., annotated with an extent.
 class Located a where
-  locate :: a -> Extent -> a               -- ^ Annotates an object with a location.
-  locate_opt :: a -> Maybe Extent -> a     -- ^ Same as locate, with an optional location.
-  clear_location :: a -> a                 -- ^ Removes all location annotations.
-  location :: a -> Maybe Extent            -- ^ Returns the location associated to an object, if any.
+  -- | Annotate an object with an extent.
+  locate :: a -> Extent -> a
+  
+  -- | Like 'locate', but the extent is optional.
+  locate_opt :: a -> Maybe Extent -> a
+  
+  -- | Remove all extent annotations.
+  clear_location :: a -> a
+  
+  -- | Return the extent associated with an object, if any.
+  location :: a -> Maybe Extent
 
